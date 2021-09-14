91785c7a0aad348edffb81d00bd21790eceef655
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-09-14 20:59:17 +0300

    lab38

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
 import Head from 'next/head'
+
 import Header from '../components/Header'
 import Main from '../components/Main'
-import Footer from '../components/Footer'
 
-export default function Home() {
+export default function CookieStandAdmin() {
+
   return (
-    <div className="">
+    <div className="bg-green-50">
+      
       <Head>
         <title>Cookie Stand Admin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        <Header />
+      <Header />
 
-        <Main />
+      <Main />
 
-        <Footer />
 
     </div>
   )
